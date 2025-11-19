6312988b44e9188768796c5362dd307527218f46
felipedevpips-max <felipedevpips@gmail.com>
2025-11-19 16:37:13 -0300

    adicionando funcionalidades a API

--- js/api.js
+++ js/api.js
@@ -25,7 +25,37 @@ const api = { //api fake para interação
             alert('Erro ao buscar pensamentos')
             throw error
         }
-    }
+    },
+
+     async buscarPensamentoId(id) { //usando id poruqe ele e um identificador unico
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${id}`)
+            return await response.json()
+        }
+         catch {
+            alert('Erro ao buscar pensamentos')
+            throw error
+        }
+    },
+
+    async editarPensamento(pensamento) { //concatenando pensamento com o id
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {//requisição
+                method: 'PUT',
+                headers: { //cabeçalho da requisição 
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(pensamento)//conteudo passado
+            })
+            return await response.json()
+        }
+         catch {
+            alert('Erro ao editar pensamento')
+            throw error
+        }
+    },
+
+
 }
 
 export default api;
