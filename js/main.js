4e85ccfcb9365b7625418056a7f1c33b5bb75628
felipedevpips-max <felipedevpips@gmail.com>
2025-11-18 21:39:58 -0300

    cadastrando novas informaçoes

--- js/main.js
+++ js/main.js
@@ -1,27 +1,24 @@
 import ui from './ui.js'
 import api from './api.js'
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
     ui.renderizarPensamentos()
-
-    const formularioPensamento = document.getElementById('pensamento-form')
-    formularioPensamento.addEventListener('submit', manipularSubmitFormulario)
-}) //quando a pagina carrega
-
-async function manipularSubmitFormulario(event) {
-    event.preventDefault();
-    const id = document.getElementById('pensamento-id').value
-    const conteudo = document.getElementById('pensamento-counteudo').value
-    const autoria = document.getElementById('pensamento-autoria').value
-
-    try {
-        await api.salvarPensamento({conteudo, autoria})
-        ui.renderizarPensamentos()
-        
-    } catch (error) {
-        alert('Error ao salvar pensamento')
-        
-    }
-
     
+    const formularioPensamento = document.getElementById("pensamento-form")
+    formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
+})
+
+async function manipularSubmissaoFormulario(event) {
+    event.preventDefault()
+    const id = document.getElementById("pensamento-id").value
+    const conteudo = document.getElementById("pensamento-conteudo").value
+    const autoria = document.getElementById("pensamento-autoria").value
+    
+    try{
+        await api.salvarPensamento({ conteudo, autoria })
+        ui.renderizarPensamentos()
+    }
+    catch {
+        alert("Erro ao salvar pensamento")
+    }
 }
--- js/ui.js
+++ js/ui.js
@@ -2,7 +2,7 @@ import api from './api.js' //importando os dados da API
 
 const ui = {
     async renderizarPensamentos() {
-        const listaPensamentos = document.getElementById('lista-pensamentos')
+        const listaPensamentos = document.getElementById("lista-pensamentos")
 
         try {
             const pensamentos = await api.buscarPensamentos()
@@ -10,29 +10,27 @@ const ui = {
         }
         catch {
             alert('Erro ao renderizar pensamentos')
-
         }
     },
-
+    
     adicionarPensamentoNaLista(pensamento) {
-        const listaPensamentos = document.getElementById('lista-pensamentos')
-        const li = document.createElement('li')
-        li.setAttribute('data-id', pensamento.id)
-        li.classList.add('li-pensamento')
+        const listaPensamentos = document.getElementById("lista-pensamentos")
+        const li = document.createElement("li")
+        li.setAttribute("data-id", pensamento.id)
+        li.classList.add("li-pensamento")
 
-
-        const iconeAspas = document.createElement('img')
-        iconeAspas.src = 'assets/imagens/aspas-azuis.png'
+        const iconeAspas = document.createElement("img")
+        iconeAspas.src = "assets/imagens/aspas-azuis.png"
         iconeAspas.alt = "Aspas azuis"
         iconeAspas.classList.add("icone-aspas")
 
-        const pensamentoConteudo = document.createElement('div')
+        const pensamentoConteudo = document.createElement("div")
         pensamentoConteudo.textContent = pensamento.conteudo
-        pensamentoConteudo.classList.add('pensamento-conteudo')
+        pensamentoConteudo.classList.add("pensamento-conteudo")
 
-        const pensamentoAutoria = document.createElement('div')
-        pensamentoAutoria.textContent = pensamento.conteudo
-        pensamentoAutoria.classList.add('pensamento-autoria')
+        const pensamentoAutoria = document.createElement("div")
+        pensamentoAutoria.textContent = pensamento.autoria
+        pensamentoAutoria.classList.add("pensamento-autoria")
 
         li.appendChild(iconeAspas)
         li.appendChild(pensamentoConteudo)
