7054592d02a86142506e6fb31a0c9287b56bc322
felipedevpips-max <felipedevpips@gmail.com>
2025-11-18 21:21:52 -0300

    refatorando codigo

--- js/api.js
+++ js/api.js
@@ -8,6 +8,23 @@ const api = { //api fake para interação
             alert('Erro ao buscar pensamentos')
             throw error
         }
+    },
+
+     async salvarPensamento(pensamento) {
+        try {
+            const response = await fetch('http://localhost:3000/pensamentos', {//requisição
+                method: 'POST',
+                headers: { //cabeçalho da requisição 
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(pensamento)//conteudo passado
+            })
+            return await response.json()
+        }
+         catch {
+            alert('Erro ao buscar pensamentos')
+            throw error
+        }
     }
 }
 
--- js/main.js
+++ js/main.js
@@ -2,4 +2,6 @@ import ui from './ui.js'
 
 document.addEventListener('DOMContentLoaded', () => {
     ui.renderizarPensamentos()
+
+    const formularioPensamento = document.getElementById('pensamento-form')
 }) //quando a pagina carrega
