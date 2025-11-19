a585bd54f1b390b60f34d4387d8829176ef0498b
felipedevpips-max <felipedevpips@gmail.com>
2025-11-19 16:50:22 -0300

    corrigindo main

--- js/ui.js
+++ js/ui.js
@@ -1,6 +1,15 @@
 import api from './api.js' //importando os dados da API
 
 const ui = {
+
+    async preencherFormulario(pensamentoId){
+        const pensamento = await api.buscarPensamentoId(pensamentoId)
+        document.getElementById('pensamento-id').value = pensamento.id
+        document.getElementById('pensamento-conteudo').value = pensamento.conteudo
+        document.getElementById('pensamento-autoria').value = pensamento.autoria
+
+    },
+
     async renderizarPensamentos() {
         const listaPensamentos = document.getElementById("lista-pensamentos")
 
@@ -32,10 +41,26 @@ const ui = {
         pensamentoAutoria.textContent = pensamento.autoria
         pensamentoAutoria.classList.add("pensamento-autoria")
 
+        const botaoEditar = document.createElement("button")
+        botaoEditar.classList.add("botao-editar")
+        botaoEditar.onclick = () => ui.preencherFormulario(pensamento.id)
+
+        const iconeEditar = document.createElement("img")
+        iconeEditar.src = "assets/imagens/icone-editar.png"
+        iconeEditar.alt = "Editar"
+        botaoEditar.appendChild(iconeEditar)
+
+        const icones = document.createElement('div')
+        botaoEditar.classList.add('icones')
+        icones.appendChild(botaoEditar)
+
+
         li.appendChild(iconeAspas)
         li.appendChild(pensamentoConteudo)
         li.appendChild(pensamentoAutoria)
+        li.appendChild(icones)
         listaPensamentos.appendChild(li)
+
     },
 
     limparFormulario() {
